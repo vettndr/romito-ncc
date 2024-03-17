1345852c22d07a284ba3bca89bd5f9ca12ab478a
Andrea Vettore <andreaweb1990@gmail.com>
2024-03-17 14:49:33 +0100

    chore: packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,27 @@
+import screens from "./utils/screens"
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
 
-  modules: ["@nuxtjs/tailwindcss"],
-
   css: ["~/assets/scss/app.scss"],
+
+  modules: [
+    //Doc: https://tailwindcss.nuxtjs.org/
+    "@nuxtjs/tailwindcss",
+    //Doc: https://github.com/mvrlin/nuxt-viewport
+    "nuxt-viewport",
+  ],
+
+  viewport: {
+    breakpoints: {
+      ...screens,
+    },
+    defaultBreakpoints: {
+      desktop: "md",
+      tablet: "sm",
+      mobile: "xs",
+    },
+    fallbackBreakpoint: "md",
+  },
 })
