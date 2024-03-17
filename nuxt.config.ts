aa5206697ce5fd75b3bca42bd3dae59ca39b2986
Andrea Vettore <andreaweb1990@gmail.com>
2024-03-17 17:32:05 +0100

    feat: icons setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,8 @@
+import Components from "unplugin-vue-components/vite"
+import IconsResolver from "unplugin-icons/resolver"
+import Icons from "unplugin-icons/vite"
+import { HeadlessUiResolver } from "unplugin-vue-components/resolvers"
+import { FileSystemIconLoader } from "unplugin-icons/loaders"
 import screens from "./utils/screens"
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
@@ -58,5 +63,33 @@ export default defineNuxtConfig({
   fonts: {
     provider: "google",
     families: [{ name: "Jost", provider: "google", weights: [400, 600, 700] }],
+  },
+
+  vite: {
+    plugins: [
+      Components({
+        resolvers: [
+          IconsResolver({
+            prefix: "v",
+            customCollections: ["icons"],
+          }),
+          HeadlessUiResolver(),
+        ],
+        dts: true,
+      }),
+      Icons({
+        customCollections: {
+          icons: FileSystemIconLoader("./icons", (svg) => {
+            const viewBox = /viewBox="\d+ \d+ (\d+) (\d+)"/gi.exec(svg)
+            if (!viewBox) return svg
+            const w = viewBox?.[1]
+            const h = viewBox?.[2]
+            svg = svg.replace(/width=".*?"/, `width="${w}" `)
+            svg = svg.replace(/height=".*?"/, `height="${h}" `)
+            return svg
+          }),
+        },
+      }),
+    ],
   },
 })
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+// const { data: dataHome, error: errorHome } = await useFetch(
+//   `/api/fetchHome/home`
+// )
+
+// if (errorHome.value) {
+//   throw createError({
+//     statusCode: 404,
+//     statusMessage: "Page Not Found",
+//     fatal: true,
+//   })
+// }
+
+// useSeoMeta({
+//   title: dataHome.value?.seo?.meta_title,
+//   ogTitle: dataHome.value?.seo?.meta_title,
+//   description: dataHome.value?.seo?.meta_description,
+//   ogDescription: dataHome.value?.seo?.meta_description,
+// })
+</script>
+
+<template>
+  <div class="homepage min-h-screen">
+    <!-- <OrganismsHerobanner
+      v-if="dataHome?.herobanner"
+      v-bind="dataHome?.herobanner"
+    />
+    <VDynamicContent v-if="dataHome?.content" :components="dataHome?.content" /> -->
+  </div>
+</template>
