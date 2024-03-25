cf5f48b8fd7d8c87a9c452207432b65149f9a0df
Andrea Vettore <andreaweb1990@gmail.com>
2024-03-25 14:35:17 +0100

    feat: footer details

--- components/footer/Default.vue
+++ components/footer/Default.vue
@@ -8,23 +8,30 @@ const props = defineProps<FooterDefaultProps>()
     <div class="grid-standard py-8">
       <div class="col-span-full pb-6 lg:pb-0 lg:col-span-3">
         <p class="text-primary font-medium">
-          frase per noleggio conducente info paragrafo breve descrizione
+          Romito Roberto NCC è il tuo noleggio con conducente di fiducia.
         </p>
       </div>
       <div
         class="col-span-full pb-6 sm:col-span-2 lg:col-span-2 lg:col-start-6"
       >
         <p>
+          <span class="text-primary font-medium">Sede Legale:</span><br />
+          Via Svizzera, 6<br />
+          35127, Padova (PD)<br />
+          <span class="text-primary font-medium">P.Iva:</span> 05019830289
+        </p>
+        <br />
+        <p>
           <span class="text-primary font-medium">Sede:</span><br />
-          Via indirizzo, 23<br />
-          33333, Citta (RO)<br />
-          <span class="text-primary font-medium">P.Iva:</span> 333333333
+          Via Sabbionara, 76<br />
+          35045, Ospedaletto Euganeo (PD)
         </p>
       </div>
       <div class="col-span-full sm:col-span-2 lg:col-span-3 lg:col-start-9">
         <p>
           <span class="text-primary font-medium">Telefono:</span><br />
-          <NuxtLink to="tel:+3934033333333">+340 33333333</NuxtLink><br />
+          <NuxtLink to="tel:+393294164902">+329 4164902</NuxtLink><br />
+          <NuxtLink to="tel:+393881222641">+388 1222641</NuxtLink><br />
           <span class="text-primary font-medium">Email: </span>
           <NuxtLink to="mailto:romitorobertoncc@gmail.com"
             >romitorobertoncc@gmail.com</NuxtLink
